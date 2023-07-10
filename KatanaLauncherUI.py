bd6a5393e1f438bebce87da3eb823a62460958e6
dpsencik <133698718+dpsencik@users.noreply.github.com>
2023-07-10 12:25:48 -0500

    Now correctly launches more than one Katana instance
    Each temp.sh is created separately and deletes themselves after that terminal instance is closed.

--- KatanaLauncherUI.py
+++ KatanaLauncherUI.py
@@ -23,6 +23,7 @@ class KatanaLauncher(QtWidgets.QMainWindow):
         self.renderer_CB.currentTextChanged.connect(self.renderer_changed)
         self.run_BTN.pressed.connect(self.launch)
         self.settings_BTN.pressed.connect(self.settingsWindow)
+        self.instancesRunning = 1
 
     def location_on_the_screen(self):
         qtRectangle = self.frameGeometry()
@@ -50,6 +51,7 @@ class KatanaLauncher(QtWidgets.QMainWindow):
         
 
     def refreshInputs(self):
+
         #clear scripts
         while self.scripts_layout.count():
             child = self.scripts_layout.takeAt(0)
@@ -71,6 +73,7 @@ class KatanaLauncher(QtWidgets.QMainWindow):
         self.renderer_CB.addItems(klm.getRenderers())
 
     def settingsWindow(self):
+        settings.updatePaths()
         settings.show()
 
     def launch(self):
@@ -84,13 +87,19 @@ class KatanaLauncher(QtWidgets.QMainWindow):
             for i in range (self.scripts_layout.count() - 1):
                 script = self.scripts_layout.itemAt(i).widget()
                 if script.isChecked():
-                    command += Path(klm.paths['scripts_path'] + '/' + script.text()).read_text()
-            command += Path(klm.paths['scripts_path'] + '/Renderers/' + renderer + '.sh').read_text()
+                    command += Path(klm.SCRIPT_PATH + '/' + script.text()).read_text()
+            command += Path(klm.SCRIPT_PATH + '/Renderers/' + renderer + '.sh').read_text()
+            #removes the temp.sh after katana closes
+            command += "rm -- \"$0\""
+            
             #Create a temp launch file and use it in a new terminal
-            with open('temp.sh', 'w') as tempLaunch:
+
+            with open('temp%s.sh' % self.instancesRunning, 'w') as tempLaunch:
                 tempLaunch.write(command)
-                os.system('gnome-terminal --window -- bash -c "bash -i temp.sh"')
-            os.remove('temp.sh')
+                os.system('gnome-terminal --window -- bash -c \"bash -i temp%s.sh\"' % self.instancesRunning)
+                self.instancesRunning = self.instancesRunning + 1
+                
+            #os.remove('temp.sh')
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
